909caebd1fd46ab2e386100bbd8c338c0e74ed85
ostheperson <seyiojo13@gmail.com>
2023-11-18 20:59:40 +0100

    add: api endpoint to handle multiple emails

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -6,8 +6,9 @@ import (
 	"log"
 	"net/http"
 
-	emailVerifier "github.com/AfterShip/email-verifier"
 	"github.com/julienschmidt/httprouter"
+
+	emailVerifier "github.com/AfterShip/email-verifier"
 )
 
 func GetEmailVerification(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -29,12 +30,49 @@ func GetEmailVerification(w http.ResponseWriter, r *http.Request, ps httprouter.
 	}
 
 	_, _ = fmt.Fprint(w, string(bytes))
+}
 
+func GetEmailsVerification(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	type RequestBody struct {
+		Emails []string `json:"emails"`
+	}
+	var results []emailVerifier.Result
+	var requestBody RequestBody
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&requestBody)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	verifier := emailVerifier.NewVerifier()
+	for _, email := range requestBody.Emails {
+		ret, err := verifier.Verify(email)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			continue
+		}
+		if !ret.Syntax.Valid {
+			_, _ = fmt.Fprint(w, "email address syntax is invalid")
+			continue
+		}
+		results = append(results, *ret)
+	}
+	responseMap := map[string]interface{}{
+		"data": results,
+	}
+	errx := json.NewEncoder(w).Encode(responseMap)
+	if errx != nil {
+		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
 	router := httprouter.New()
 
+	router.POST("/v1/bulk/verifications", GetEmailsVerification)
 	router.GET("/v1/:email/verification", GetEmailVerification)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
